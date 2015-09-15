3c8f84784367caf82d4fb6a9232b7c2617e641f4
Andres Hernandez <tronwares@gmail.com>
2015-09-15 04:13:13 -0500

    Agregando Json - Grupos desde una api

--- js/app.js
+++ js/app.js
@@ -1,4 +1,19 @@
-var app = angular.module('meetupApp',[]);
+var app = angular.module('meetupApp', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider){
+	$routeProvider
+		.when('/', {
+		templateUrl: 'views/home.html',
+		controller: 'meetupsViewController'
+	})
+		.when('/grupos', {
+		templateUrl: 'views/grupos.html',
+		controller: 'meetupController'
+	})
+		.otherwise({
+		redirectTo: 'views/error.html'
+	});
+}]);
 
 app.controller('homeViewController', ['$scope',function($scope){
 	$scope.appTitle="Logo-Meetup"
@@ -9,8 +24,25 @@ app.controller('meetupsViewController', ['$scope',function($scope){
 	$scope.meetups = [
 		{id: 'meetup-01',name: 'Bogotá Senderismo en la naturaleza Meetup',subTitle:'Somos 810 Caminantes del Retorno',img: 'img/439070461.jpeg'},
 		{id: 'meetup-02',name: 'Agiles Colombia',subTitle:'Somos 2,497 Agiles',img: 'img/437404685.jpeg'},
+		{id: 'meetup-03',name: 'Bogotá Futbol 5 Meetup',subTitle:'Somos 32 Futboleros',img: 'img/440274279.jpeg'},
+		{id: 'meetup-01',name: 'Bogotá Senderismo en la naturaleza Meetup',subTitle:'Somos 810 Caminantes del Retorno',img: 'img/439070461.jpeg'},
+		{id: 'meetup-02',name: 'Agiles Colombia',subTitle:'Somos 2,497 Agiles',img: 'img/437404685.jpeg'},
 		{id: 'meetup-03',name: 'Bogotá Futbol 5 Meetup',subTitle:'Somos 32 Futboleros',img: 'img/440274279.jpeg'}
 
 	];
 
+}]);
+
+app.controller('meetupController',['$scope','$http',function($scope,$http){
+	$scope.meetupsGru = [];
+	
+	$http.get("http://jsonplaceholder.typicode.com/photos")
+		.success(function(data){
+			console.log(data);
+		$scope.meetupsGru= data;
+		})
+		.error(function(err){
+		
+		})
+	
 }]);
